e402df5a6e439a5571ad6d5e41c2f1c40d596068
ianlandingin <iclandingin28@gmail.com>
2023-11-08 09:09:57 +0800

    added mongodb configurations created env variables

--- movies/src/main/java/com/landingintechsolutions/movies/MoviesApplication.java
+++ movies/src/main/java/com/landingintechsolutions/movies/MoviesApplication.java
@@ -2,7 +2,6 @@ package com.landingintechsolutions.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,11 +10,6 @@ public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String apiIndex() {
-		return "Hello World!";
 	}
 
 }
